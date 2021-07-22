3bf813d2ce5a825df6055449af8e0f623b08e66c
fernandoobarbosa <fernandobarbosa1697@gmail.com>
2021-07-22 15:08:04 -0300

    Initial commit

---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,193 @@
+﻿using IronPython.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Serialization;
+using static WebApplication2.PreLista;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var correios = new Correios.AtendeClienteClient();
+
+            var binding = correios.Endpoint.Binding;
+            if (binding is BasicHttpBinding)
+            {
+                ((BasicHttpBinding)binding).MaxReceivedMessageSize = 100000000;
+            }
+            else if (binding is WSHttpBinding)
+            {
+                ((WSHttpBinding)binding).MaxReceivedMessageSize = 1000000;
+            }
+            else
+            {
+                // outros tipos
+                var newBinding = new CustomBinding(binding);
+                for (var i = 0; i < newBinding.Elements.Count; i++)
+                {
+                    if (newBinding.Elements[i] is TransportBindingElement)
+                    {
+                        ((TransportBindingElement)newBinding.Elements[i]).MaxReceivedMessageSize = 1000000;
+                    }
+                }
+
+                correios.Endpoint.Binding = newBinding;
+            }
+
+            try
+            {
+
+                //var cliente = correios.buscaCliente("9992157880", "0067599079", "sigep", "n5f9t8");
+                //var servicos = correios.buscaServicos("9992157880", "0067599079", "sigep", "n5f9t8");
+               
+                
+                string usuario = "sigep";
+                string senha = "n5f9t8";
+
+                //var estaDisponivel = correios.verificaDisponibilidadeServico(17000190, servicos[1].codigo, "09580620", "09510101", usuario, senha);
+               
+               
+                //var idServico = cliente;
+
+                // var etiquetas = correios.solicitaEtiquetas("C", "34028316000103", servicos[0].id, 2, "sigep", "n5f9t8");
+                //Antes de enviar os objetos da lista parapostagem a PLP deverá ser fechada
+                // var xmlPlp = correios.solicitaXmlPlp(1234, "sigep", "n5f9t8");
+                var cliente = correios.buscaCliente("9992157880", "0067599079", usuario, senha);
+
+                var servicos = cliente.contratos[0].cartoesPostagem[0].servicos;
+
+                //an instance of the class Learning is created
+                Correioslog c = new Correioslog();
+                c.Remetente = new Remetente();
+                c.Plp = new Plp();
+                c.ObjetoPostal = new List<ObjetoPostal>();
+                c.TipoArquivo = "Postagem";
+
+                var cep = correios.consultaCEP("09510000");
+                
+                c.Plp.CartaoPostagem = 0067599079;
+                c.Remetente.NumeroContrato = 9992157880;
+                c.Remetente.NumeroDiretoria = 72;
+                c.Remetente.CodigoAdministrativo = 17000190;
+                c.Remetente.NomeRemetente = "Correios Teste";
+                c.Remetente.LogradouroRemetente = cep.end;
+                c.Remetente.NumeroRemetente = 123;
+                c.Remetente.ComplementoRemetente = cep.complemento2;
+                c.Remetente.BairroRemetente = cep.bairro;
+                c.Remetente.CepRemetente = int.Parse(cep.cep);
+                c.Remetente.CidadeRemetente = cep.cidade;
+                c.Remetente.UfRemetente = cep.uf;
+
+                //var servicos = correios.buscaServicos("9992157880", "0067599079", "sigep", "n5f9t8");
+                
+                var etiquetas = correios.solicitaEtiquetas("C", "34028316000103", servicos[0].id, 2, usuario, senha);
+
+             
+                string[] numeroObjetos = etiquetas.Split(' ',',');
+
+                string[] etiquetasArray = etiquetas.Split(',');
+
+                
+
+                List<string> vs = new List<string>();
+                
+                foreach(string etiqueta in etiquetasArray)
+                {
+                    string trimmed = String.Concat(etiqueta.Where(et => !Char.IsWhiteSpace(et)));
+                    vs.Add(trimmed);
+                }
+
+                var digito = correios.geraDigitoVerificadorEtiquetas(etiquetasArray, usuario,senha);
+
+                //Importante: Deverá constar no códigodoobjeto:
+                //Sigla do Tipo Postal(ex: DW) +númerodo objeto + digito verificador + BR.Exemplo: DW123456785BR
+
+                string etiquetaTesteCancelamento = "";
+
+
+                for (int i = 0; i < etiquetasArray.Length; i++)
+                {
+                    ObjetoPostal objetoPostal = new ObjetoPostal();
+
+                    string etiquetaMontada = numeroObjetos[i * 2] + digito[i] + "BR";
+                    etiquetaTesteCancelamento = etiquetaMontada;
+                    //objetoPostal.NumeroEtiqueta = numeroObjetos[i *2];
+                    objetoPostal.NumeroEtiqueta = etiquetaMontada;
+                    objetoPostal.CodigoServicoPostagem = ((int)servicos[0].id);
+                    objetoPostal.Cubagem = 0.00;
+                    objetoPostal.Peso = 10;
+                    //objetoPostal.Destinatario = new Destinatario();
+                    Destinatario destinatario = new Destinatario();
+                    
+                    destinatario.NumeroEndDestinatario = 123;
+                    destinatario.LogradouroDestinatario = "Rua das ruas";
+                    destinatario.NomeDestinatario = "Venturi";
+                    objetoPostal.Destinatario = destinatario;
+
+                    Nacional nacional = new Nacional();
+
+                    var cepDestinatario = correios.consultaCEP("09510101");
+                    nacional.BairroDestinatario = cepDestinatario.bairro;
+                    nacional.CidadeDestinatario = cepDestinatario.cidade;
+                    nacional.UfDestinatario = cepDestinatario.uf;
+                    nacional.CepDestinatario = int.Parse(cepDestinatario.cep);
+                    objetoPostal.Nacional = nacional;
+
+                 
+                    c.ObjetoPostal.Add(objetoPostal);
+                }
+
+                
+             
+                //an instance of the XmlSerializer class is created
+                XmlSerializer inst = new XmlSerializer(typeof(Correioslog));
+                //an instance of the TextWriter class is created to write the converted XML string to the file
+                TextWriter writer = new StreamWriter(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory.ToString()) + @"\tb_cid.xml");
+                inst.Serialize(writer, c);
+                writer.Close();
+
+                //XmlDocument doc = new XmlDocument();
+                //doc.Load(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory.ToString()) + @"\tb_cid.xml");
+
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory.ToString()) + @"\tb_cid.xml");
+                StringWriter sw = new StringWriter();
+                XmlTextWriter tx = new XmlTextWriter(sw);
+                doc.WriteTo(tx);
+               
+
+                //var idPlp = correios.fechaPlp(sw.ToString(), 1234, "0067599079", etiquetas, usuario,senha);
+
+                var PlpVariosServicos = correios.fechaPlpVariosServicos(sw.ToString(), cliente.id, "0067599079",vs.ToArray(), usuario, senha);
+                
+
+                //var obterPlp = correios.solicitaXmlPlp(idPlp, "sigep", "n5f9t8");
+                
+                //cancelando com a etiqueta com digito verificador
+
+                var cancelamento = correios.cancelarObjeto(PlpVariosServicos,etiquetaTesteCancelamento, usuario,senha);
+
+                var teste = correios.solicitaXmlPlp(PlpVariosServicos, usuario, senha);
+                var x = correios.solicitaXmlPlp(PlpVariosServicos, usuario, senha);
+
+                //var idPlp = correios.fechaPlp(sw.ToString(), 1234, "0067599079", etiquetas, usuario, senha);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ WebApplication2/PreLista.cs
@@ -0,0 +1,269 @@
+﻿using HtmlParserSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebApplication2
+{
+    public class PreLista
+    {
+	
+
+		[XmlRoot(ElementName = "plp")]
+		public class Plp
+		{
+
+			[XmlElement(ElementName = "id_plp")]
+			public object IdPlp { get; set; }
+
+			[XmlElement(ElementName = "valor_global")]
+			public object ValorGlobal { get; set; }
+
+			[XmlElement(ElementName = "mcu_unidade_postagem")]
+			public object McuUnidadePostagem { get; set; }
+
+			[XmlElement(ElementName = "nome_unidade_postagem")]
+			public object NomeUnidadePostagem { get; set; }
+
+			[XmlElement(ElementName = "cartao_postagem")]
+			public int CartaoPostagem { get; set; }
+		}
+
+		[XmlRoot(ElementName = "remetente")]
+		public class Remetente
+		{
+
+			[XmlElement(ElementName = "numero_contrato")]
+			public double NumeroContrato { get; set; }
+
+			[XmlElement(ElementName = "numero_diretoria")]
+			public int NumeroDiretoria { get; set; }
+
+			[XmlElement(ElementName = "codigo_administrativo")]
+			public int CodigoAdministrativo { get; set; }
+
+			[XmlElement(ElementName = "nome_remetente")]
+			public string NomeRemetente { get; set; }
+
+			[XmlElement(ElementName = "logradouro_remetente")]
+			public string LogradouroRemetente { get; set; }
+
+			[XmlElement(ElementName = "numero_remetente")]
+			public int NumeroRemetente { get; set; }
+
+			[XmlElement(ElementName = "complemento_remetente")]
+			public string ComplementoRemetente { get; set; }
+
+			[XmlElement(ElementName = "bairro_remetente")]
+			public string BairroRemetente { get; set; }
+
+			[XmlElement(ElementName = "cep_remetente")]
+			public int CepRemetente { get; set; }
+
+			[XmlElement(ElementName = "cidade_remetente")]
+			public string CidadeRemetente { get; set; }
+
+			[XmlElement(ElementName = "uf_remetente")]
+			public string UfRemetente { get; set; }
+
+			[XmlElement(ElementName = "telefone_remetente")]
+			public double TelefoneRemetente { get; set; }
+
+			[XmlElement(ElementName = "fax_remetente")]
+			public double FaxRemetente { get; set; }
+
+			[XmlElement(ElementName = "email_remetente")]
+			public string EmailRemetente { get; set; }
+
+			[XmlElement(ElementName = "celular_remetente")]
+			public object CelularRemetente { get; set; }
+
+			[XmlElement(ElementName = "cpf_cnpj_remetente")]
+			public object CpfCnpjRemetente { get; set; }
+
+			[XmlElement(ElementName = "ciencia_conteudo_proibido")]
+			public string CienciaConteudoProibido { get; set; }
+		}
+
+		[XmlRoot(ElementName = "destinatario")]
+		public class Destinatario
+		{
+
+			[XmlElement(ElementName = "nome_destinatario")]
+			public string NomeDestinatario { get; set; }
+
+			[XmlElement(ElementName = "telefone_destinatario")]
+			public double TelefoneDestinatario { get; set; }
+
+			[XmlElement(ElementName = "celular_destinatario")]
+			public double CelularDestinatario { get; set; }
+
+			[XmlElement(ElementName = "email_destinatario")]
+			public string EmailDestinatario { get; set; }
+
+			[XmlElement(ElementName = "logradouro_destinatario")]
+			public string LogradouroDestinatario { get; set; }
+
+			[XmlElement(ElementName = "complemento_destinatario")]
+			public string ComplementoDestinatario { get; set; }
+
+			[XmlElement(ElementName = "numero_end_destinatario")]
+			public int NumeroEndDestinatario { get; set; }
+
+			[XmlElement(ElementName = "cpf_cnpj_destinatario")]
+			public object CpfCnpjDestinatario { get; set; }
+		}
+
+		[XmlRoot(ElementName = "nacional")]
+		public class Nacional
+		{
+
+			[XmlElement(ElementName = "bairro_destinatario")]
+			public string BairroDestinatario { get; set; }
+
+			[XmlElement(ElementName = "cidade_destinatario")]
+			public string CidadeDestinatario { get; set; }
+
+			[XmlElement(ElementName = "uf_destinatario")]
+			public string UfDestinatario { get; set; }
+
+			[XmlElement(ElementName = "cep_destinatario")]
+			public int CepDestinatario { get; set; }
+
+			[XmlElement(ElementName = "codigo_usuario_postal")]
+			public object CodigoUsuarioPostal { get; set; }
+
+			[XmlElement(ElementName = "centro_custo_cliente")]
+			public object CentroCustoCliente { get; set; }
+
+			[XmlElement(ElementName = "numero_nota_fiscal")]
+			public int NumeroNotaFiscal { get; set; }
+
+			[XmlElement(ElementName = "serie_nota_fiscal")]
+			public object SerieNotaFiscal { get; set; }
+
+			[XmlElement(ElementName = "valor_nota_fiscal")]
+			public object ValorNotaFiscal { get; set; }
+
+			[XmlElement(ElementName = "natureza_nota_fiscal")]
+			public object NaturezaNotaFiscal { get; set; }
+
+			[XmlElement(ElementName = "descricao_objeto")]
+			public object DescricaoObjeto { get; set; }
+
+			[XmlElement(ElementName = "valor_a_cobrar")]
+			public double ValorACobrar { get; set; }
+		}
+
+		[XmlRoot(ElementName = "servico_adicional")]
+		public class ServicoAdicional
+		{
+
+			[XmlElement(ElementName = "codigo_servico_adicional")]
+			public List<int> CodigoServicoAdicional { get; set; }
+
+			[XmlElement(ElementName = "valor_declarado")]
+			public double ValorDeclarado { get; set; }
+		}
+
+		[XmlRoot(ElementName = "dimensao_objeto")]
+		public class DimensaoObjeto
+		{
+
+			[XmlElement(ElementName = "tipo_objeto")]
+			public int TipoObjeto { get; set; }
+
+			[XmlElement(ElementName = "dimensao_altura")]
+			public double DimensaoAltura { get; set; }
+
+			[XmlElement(ElementName = "dimensao_largura")]
+			public double DimensaoLargura { get; set; }
+
+			[XmlElement(ElementName = "dimensao_comprimento")]
+			public double DimensaoComprimento { get; set; }
+
+			[XmlElement(ElementName = "dimensao_diametro")]
+			public double DimensaoDiametro { get; set; }
+		}
+
+		[XmlRoot(ElementName = "objeto_postal")]
+		public class ObjetoPostal
+		{
+
+			[XmlElement(ElementName = "numero_etiqueta")]
+			public string NumeroEtiqueta { get; set; }
+
+			[XmlElement(ElementName = "codigo_objeto_cliente")]
+			public object CodigoObjetoCliente { get; set; }
+
+			[XmlElement(ElementName = "codigo_servico_postagem")]
+			public int CodigoServicoPostagem { get; set; }
+
+			[XmlElement(ElementName = "cubagem")]
+			public double Cubagem { get; set; }
+
+			[XmlElement(ElementName = "peso")]
+			public int Peso { get; set; }
+
+			[XmlElement(ElementName = "rt1")]
+			public object Rt1 { get; set; }
+
+			[XmlElement(ElementName = "rt2")]
+			public object Rt2 { get; set; }
+
+			[XmlElement(ElementName = "restricao_anac")]
+			public object RestricaoAnac { get; set; }
+
+			[XmlElement(ElementName = "destinatario")]
+			public Destinatario Destinatario { get; set; }
+
+			[XmlElement(ElementName = "nacional")]
+			public Nacional Nacional { get; set; }
+
+			[XmlElement(ElementName = "servico_adicional")]
+			public ServicoAdicional ServicoAdicional { get; set; }
+
+			[XmlElement(ElementName = "dimensao_objeto")]
+			public DimensaoObjeto DimensaoObjeto { get; set; }
+
+			[XmlElement(ElementName = "data_postagem_sara")]
+			public object DataPostagemSara { get; set; }
+
+			[XmlElement(ElementName = "status_processamento")]
+			public int StatusProcessamento { get; set; }
+
+			[XmlElement(ElementName = "numero_comprovante_postagem")]
+			public object NumeroComprovantePostagem { get; set; }
+
+			[XmlElement(ElementName = "valor_cobrado")]
+			public object ValorCobrado { get; set; }
+		}
+
+		[XmlRoot(ElementName = "correioslog")]
+		public class Correioslog
+		{
+
+			[XmlElement(ElementName = "tipo_arquivo")]
+			public string TipoArquivo { get; set; }
+
+			[XmlElement(ElementName = "versao_arquivo")]
+			public DateTime VersaoArquivo { get; set; }
+
+			[XmlElement(ElementName = "plp")]
+			public Plp Plp { get; set; }
+
+			[XmlElement(ElementName = "remetente")]
+			public Remetente Remetente { get; set; }
+
+			[XmlElement(ElementName = "forma_pagamento")]
+			public object FormaPagamento { get; set; }
+
+			[XmlElement(ElementName = "objeto_postal")]
+			public List<ObjetoPostal> ObjetoPostal { get; set; }
+		}
+
+
+	}
+}
